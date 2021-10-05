b738b6507853a427b2878cd4eeb7b171035d4a08
Krebs <lucasklopess@gmail.com>
2021-10-05 17:58:48 -0300

    This is our first version of the project without testing

--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,22 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 14:05:33 by gcosta-d          #+#    #+#             */
-/*   Updated: 2021/10/05 14:06:04 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/10/05 17:52:445 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define MAX_FD 256
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+# define MAX_FD 256
+# define BUFFER_SIZE 5
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *string);
+char	*ft_strdup(const char *src);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strchr(const char *s, int c);
+
+#endif
--- get_next_line_.c
+++ get_next_line_.c
@@ -12,20 +12,51 @@
 
 #include "get_next_line.h"
 
+
+char	*format_line(int fd, char **buffer, char *swap)
+{
+	int		new_line_position;
+	char	*line_formated;
+
+	new_line_position = ft_strchr(*buffer, '\n');
+	*buffer = substr(swap, new_line_position + 1, ft_strlen(swap));
+	swap[new_line_position] = '\0';
+	line_formated = swap;
+	return (line_formated);
+}
+
+char	*get_line(int fd, char **buffer, char  *read_buffer)
+{
+	int		*read_bytes;
+	int		*swap;
+	char	*aux;
+
+	aux = ft_strchr(*buffer, '\n');
+	while (aux == NULL)
+	{
+		read_bytes = read(fd, read_buffer, BUFFER_SIZE);
+		if (read_bytes <= 0) //verificar retorno read_bytes == 0
+			return (NULL);
+		swap = ft_strjoin(*buffer, read_buffer);
+		*buffer = swap;
+		aux = ft_strchr(*buffer, '\n');
+	}
+	return (format_line(fd, buffer, swap));
+// função do format line é enviar o aaaaa/nbb e guardar o bb dentro do buffer
+}
+
+
 char	*get_next_line(int fd)
 {
 	static char	*buffer[MAX_FD];
-	int			*read_bytes;
-	int			*swap;
 	char		*read_buffer;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE == 0) //mais um ||
 		return (NULL);
 	read_buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!read_buffer)
 		return (NULL);
-	read_bytes = read(fd, read_buffer, BUFFER_SIZE);
-	if (read_bytes <= 0) //verificar retorno read_bytes == 0
-		return (NULL);
+	line = get_line(fd, &buffer[fd], read_buffer);
+	return (line);
 }
-
