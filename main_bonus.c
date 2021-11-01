b2fc6cfefded0453a50f4b3c5d43cf778eacaa97
krebscoder <lucasklopess@gmail.com>
2021-11-01 19:08:20 -0300

    main and main_bonus

--- main_bonus.c
+++ main_bonus.c
@@ -1,14 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/01 18:33:55 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/11/01 19:06:45 by lkrebs-l         ###   ########.fr       */
+/*   Created: 2021/11/01 19:05:54 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2021/11/01 19:07:32 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include "get_next_line.h"
 #include <stdio.h>
@@ -22,11 +23,16 @@ int	main(void)
 	char	*line1;
 	
 	fd = open("teste", O_RDONLY);
+	fd1 = open("teste2", O_RDONLY);
+	
+	line = get_next_line(fd);
+	line1 = get_next_line(fd1);
+	printf("%s", line);
+	printf("%s", line1);
 
-	while (line != NULL)
-	{
-		line = get_next_line(fd);
-		printf("%s", line);
-	}
+	line = get_next_line(fd);
+	line1 = get_next_line(fd1);
+	printf("%s", line);
+	printf("%s", line1);
 	return (0);
 }
