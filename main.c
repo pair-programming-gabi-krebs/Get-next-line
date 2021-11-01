487e902367a419d164e79ff3640a6f06c5924dc2
krebscoder <lucasklopess@gmail.com>
2021-11-01 18:38:29 -0300

    adding main

---
+++ main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/01 18:33:55 by lkrebs-l          #+#    #+#             */
+/*   Updated: 2021/11/01 18:37:58 by lkrebs-l         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+
+	line = malloc(1 * sizeof(char));
+	fd = open("teste", 256);
+	while (line != NULL)
+	{
+		free(line);
+		line = get_next_line(fd);
+		printf("%s", line);
+	}
+	return (0);
+}
