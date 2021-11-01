8d28a7f5304229cd73eea994943930cf91430303
krebscoder <lucasklopess@gmail.com>
2021-11-01 18:48:29 -0300

    changing main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lkrebs-l <lkrebs-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 18:33:55 by lkrebs-l          #+#    #+#             */
-/*   Updated: 2021/11/01 18:37:58 by lkrebs-l         ###   ########.fr       */
+/*   Updated: 2021/11/01 18:47:56 by lkrebs-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(void)
 	char	*line;
 
 	line = malloc(1 * sizeof(char));
-	fd = open("teste", 256);
+	fd = open("teste", O_RDONLY);
 	while (line != NULL)
 	{
 		free(line);
